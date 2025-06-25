90eef8fe8c93af80f153a97a325aadf85403476a
xjoeshadows <48157884+xjoeshadows@users.noreply.github.com>
2025-06-25 15:35:45 -0400

    Update ContextAPIEnrich-IPs-Timestamps-CSV.py

--- tools/ContextAPI/ContextAPIEnrich-IPs-Timestamps-CSV.py
+++ tools/ContextAPI/ContextAPIEnrich-IPs-Timestamps-CSV.py
@@ -8,7 +8,7 @@ import csv
 
 # --- Configuration ---
 api_url_base = "https://api.spur.us/v2/context/"
-api_token = "HNRDfEWuYGCvuD3I1hCPsK"  # Replace with your actual API token
+api_token = "YOURTOKENHERE"  # Replace with your actual API token
 default_output_file = "ip_data.csv"  # Changed default to CSV
 
 # --- Functions ---
